27f20a42ae367dcc22bc6d76afb97d52431696e5
nikita-p <nikita.petrov.1997@gmail.com>
2015-12-01 22:30:35 +0600

    OK

--- strings.cpp
+++ strings.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct WinString{
@@ -30,7 +31,6 @@ struct WinString* absolutely_str()
     char* x = new char [255];
     cin >> x;
     struct WinString* s = cr_n_str(x); //Создали строку
-    delete x;
     return s;
 }
 
@@ -59,22 +59,73 @@ int search(struct WinString* str, char* part, int index)
         if (str->data[i]==part[j])
             j++;
         else
-            j=0;
+            while (j>0 && str->data[i]!=part[j-1])
+                j--;
         if (j==len_part)
         {
-            j==0;
+            j=0;
             coin++;
         }
     }
     return coin;
 }
 
+int ch(char l, char m)
+{
+    char s;
+    ifstream  in("table.txt");
+    while(s!= l && s!=m)
+        in >> s;
+    if (s==l && s==m)
+        return 0;
+    if (s==l)
+        return 1;
+    if (s==m)
+        return -1;
+    return 0;
+}
+
+bool sort (struct WinString* a, struct WinString* b)
+{
+    int grea;
+    int min_len = a->len;
+    if(min_len>b->len)  min_len=b->len;
+    for(int i=0;i<min_len;i++)
+    {
+        grea = ch(a->data[i],b->data[i]);
+        if (grea==1) return true;
+        if (grea==-1) return false;
+    }
+    if (a->len < b->len)
+        return true;
+    return false;
+}
+
 int main()
 {
+    cout << "Put two words: " << endl;
     struct WinString* s_hello = absolutely_str(); //Создаю строку из того, что я ввожу
     struct WinString* s_fun_story = absolutely_str();
     struct WinString* s_talking = united_str(s_hello,s_fun_story); //Соединили строчки
-    int parties = search(s_talking, "he", 0); //Нахожу в строчке кусок
+    cout << "OK, " << s_talking->data << endl;
+    cout << "Write part of word: ";
+    struct WinString* word = absolutely_str();
+    int parties = search(s_talking, word->data, 0); //Нахожу в строчке кусок
     cout << parties << endl;
+    cout << "A>B?" << endl;
+    cout << "A: ";
+    struct WinString* s_A = absolutely_str();
+    cout << "B: ";
+    struct WinString* s_B = absolutely_str();
+    bool b = sort(s_A, s_B);
+    if (b){
+        cout << s_A->data << endl;
+        cout << s_B->data << endl;
+    }
+    if (!b)
+    {
+        cout << s_B->data << endl;
+        cout << s_A->data << endl;
+    }
     return 0;
 }
