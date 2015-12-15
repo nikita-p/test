8a2c3ee5141d36bbe2abd87e450bcf3df8512789
nikita-p <nikita.petrov.1997@gmail.com>
2015-12-15 20:38:21 +0600

    Переделал строки. Теперь, вроде бы, всё хорошо

--- strings.cpp
+++ strings.cpp
@@ -1,174 +1,167 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
-struct WinString{
-    char* data;
+const int MAX_STRING=100;
+
+struct MyString
+{
+    char* word;
     int len;
 };
 
-int length_str(char* word)
+struct MyString* add_from_string(char* inputWord) //Добавление из существущей последовательности символов
 {
-    int i=0;
-    while(word[i]!='\0')
-        i++;
-    return i;
-}
-
-struct WinString* cr_n_str(char* word)
-{
-    int len = length_str(word);
-    struct WinString* word_str = new struct WinString;
-    word_str->data=new char[len];
-    for(int i =0;i<len;i++)
-        word_str->data[i]=word[i];
-    word_str->len=len;
-    return word_str;
-}
-
-struct WinString* absolutely_str()
-{
-    char* x = new char [255];
-    cin >> x;
-    struct WinString* s = cr_n_str(x); //Создали строку
-    return s;
-}
-
-struct WinString* united_str(struct WinString* x, struct WinString* y)
-{
-    struct WinString* z = new struct WinString;
-    int old_len=x->len;
-    z->len=x->len + y->len;
-    int new_len=z->len;
-    z->data=new char[new_len];
-    for(int i=0;i<old_len;i++)
-        z->data[i]=x->data[i];
-    for(int i=old_len;i<new_len;i++)
-        z->data[i]=y->data[i-old_len];
-    z->data[new_len]='\0';
-    return z;
-}
-
-int search(struct WinString* str, char* part, int index)
-{
-    int coin=0; //Число совпадений
-    int len_part=length_str(part);
-    int j=0;
-    for(int i=index;i<str->len;i++)
+    int i = 0, len = 0;
+    struct MyString* string = new struct MyString;
+    string->word = new char [MAX_STRING];
+    while(inputWord[i]!='\0')
     {
-        if (str->data[i]==part[j])
-            j++;
-        else
-            while (j>0 && str->data[i]!=part[j-1])
-                j--;
-        if (j==len_part)
-        {
-            j=0;
-            coin++;
-        }
+        string->word[i] = inputWord[i];
+        len++; i++;
     }
-    return coin;
+    string->len = len;
+    return string;
 }
 
-int great(char l, char m)
+struct MyString* add_from_cin() //Добавление из cin'a
 {
-    char s;
-    ifstream  in("table.txt");
-    while(s!= l && s!=m)
-        in >> s;
-    if (s==l && s==m)
+    cout << "Input word: ";
+    char* inputWord;
+    inputWord = new char [MAX_STRING];
+    cin >> inputWord;
+
+    struct MyString* string = new struct MyString;
+    string = add_from_string(inputWord);
+
+    return string;
+}
+
+void print(struct MyString* string) //Печать слова
+{
+        cout << "Output word: " << string->word << endl;
+}
+
+int char_compare (char one, char two) //Сравнение символов
+{
+    if(one==two)
         return 0;
-    if (s==l)
-        return 1;
-    if (s==m)
-        return -1;
+    char t = 'a';
+    ifstream in("chars_table.txt");
+    while (t!='@') //Последний символ
+    {
+        in >> t;
+        if(t==one)
+            return 1;
+        if(t==two)
+            return -1;
+    }
     return 0;
 }
 
-bool sort (struct WinString* a, struct WinString* b)
+int string_compare(struct MyString* a, struct MyString* b) //Сравнение строк
 {
-    int grea;
-    int min_len = a->len;
-    if(min_len>b->len)  min_len=b->len;
-    for(int i=0;i<min_len;i++)
+    for(int i=0; i<a->len || i< b->len;i++)
     {
-        grea = great(a->data[i],b->data[i]);
-        if (grea==1) return true;
-        if (grea==-1) return false;
+        if(a->word[i]=='\0')
+            return 1;
+        if(b->word[i]=='\0')
+            return -1;
+        int t = char_compare(a->word[i], b->word[i]);
+        if(t == 1)
+            return 1;
+        if(t == -1)
+            return -1;
     }
-    if (a->len < b->len)
-        return true;
-    return false;
+    return 0;
 }
 
-struct WinString* replacer (struct WinString* word, struct WinString* paste, int start_pos, int number)
+void compare(struct MyString* a, struct MyString* b)
 {
-    if(start_pos + number >= word->len)
-        number = word->len - start_pos;
-    struct WinString* newString = new struct WinString;
-    newString->data=new char [word->len - number];
-    newString->len = word->len - number;
-    int index = 0;
-    for(int i=0;i<word->len;i++)
-        if(i<start_pos || i>(start_pos + number))
-        {
-            newString->data[index]=word->data[i];
-            index++;
-        }
-    struct WinString* addString = new struct WinString;
-    addString->data=new char [newString->len + paste->len];
-    addString->len=newString->len + paste->len;
-    for(int i=0;i< addString->len;i++)
+    int t = string_compare(a,b);
+    cout << "It is right: " << endl;
+    if(t==1 || t==0)
     {
-        if(start_pos!=i)
-        {
-            addString->data[index]=newString->data[i];
-            index++;
-        }
-        if(start_pos==i)
-            for(int j=0; j< paste->len; j++)
-            {
-                addString->data[index]=paste->data[j];
-                index++;
-            }
+        print(a);
+        print(b);
     }
-    return addString;
+    if(t==-1)
+    {
+        print(b);
+        print(a);
+    }
+}
+
+struct MyString* merger(struct MyString* a, struct MyString* b) //Слияние двух строк
+{
+    struct MyString* un = new struct MyString;
+    un->len = a->len + b->len;
+    un->word = new char [un->len];
+    for(int i=0; i<a->len;i++)
+        un->word[i] = a->word[i];
+    for(int i=0;i<b->len;i++)
+        un->word[a->len + i]=b->word[i];
+    return un;
+}
+
+int search(struct MyString* string, int position, struct MyString* piece)
+{
+    int count = 0, j=0;
+    if(position<0)
+        position = 0;
+    for(int i=position; i<string->len; i++)
+    {
+        if(string->word[i] != piece->word[j])
+            j=0;
+        if(string->word[i] == piece->word[j])
+            j++;
+        if(j == piece->len)
+        {
+            count++;
+            j=0;
+        }
+    }
+    return count;
+}
+
+struct MyString* replace(struct MyString* all, struct MyString* piece, int begin, int end) //Замена
+{
+    if(end>all->len) //условия для внештатных ситуаций
+        end = all->len;
+    if(begin>all->len)
+        begin = all->len;
+    if(begin<0)
+        begin = 0;
+    if(end<0)
+        end = 0;
+    struct MyString* newAll = new struct MyString;
+    newAll->len = all->len + piece->len + (begin - end);
+    newAll->word = new char [newAll->len];
+    for(int i=0;i<begin;i++)
+        newAll->word[i]=all->word[i];
+    for(int i=0;i<piece->len;i++)
+        newAll->word[begin+i]=piece->word[i];
+    for(int i=end; i<all->len; i++)
+        newAll->word[begin + piece->len + (i - end)] = all->word[i];
+    delete[] all;
+    return newAll;
 }
 
 int main()
 {
-    cout << "Put two words: " << endl;
-    struct WinString* s_hello = absolutely_str(); //Создаю строку из того, что я ввожу
-    struct WinString* s_fun_story = absolutely_str();
-    struct WinString* s_talking = united_str(s_hello,s_fun_story); //Соединили строчки
-    cout << "OK, " << s_talking->data << endl;
-    cout << "Write part of word: ";
-    struct WinString* word = absolutely_str();
-    int parties = search(s_talking, word->data, 0); //Нахожу в строчке кусок
-    cout << parties << endl;
-    cout << "A>B?" << endl;
-    cout << "A: ";
-    struct WinString* s_A = absolutely_str();
-    cout << "B: ";
-    struct WinString* s_B = absolutely_str();
-    bool b = sort(s_A, s_B); //Сравниваю две строчки
-    if (b){
-        cout << s_A->data << endl;
-        cout << s_B->data << endl;
-    }
-    if (!b)
-    {
-        cout << s_B->data << endl;
-        cout << s_A->data << endl;
-    }
-    char x[200], paste[200]; //Замена элементов
-    cout << "string" << endl;
-    cin >> x;
-    cout << "paste" << endl;
-    cin >> paste;
-    struct WinString* new_string = cr_n_str(x);
-    struct WinString* paste_string = cr_n_str(paste);
-    new_string = replacer (new_string, paste_string, 0 , 4 );
-    cout << new_string->data;
+    struct MyString* word = add_from_cin();
+    struct MyString* wordTwo = add_from_cin();
+    compare(word, wordTwo); //Сравнение++
+    struct MyString* merge = merger(word,wordTwo); //Слияние++
+    delete[] word;
+    delete[] wordTwo;
+    struct MyString* piece = add_from_cin();
+    print (merge);
+    int k = search(merge,0,piece); //Поиск++
+    cout <<"Number of pieces: "<< k << endl;
+    merge = replace(merge, piece, 0, 0); //Замена++
+    print (merge);
+    delete[] merge;
     return 0;
 }
