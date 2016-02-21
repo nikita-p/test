40972c5060ab990754de46b6daa9fa0504fa67af
nikita-p <nikita.petrov.1997@gmail.com>
2016-02-21 16:11:03 +0600

    Добавлена сортировка списков

--- list_class.cpp
+++ list_class.cpp
@@ -6,6 +6,28 @@ class list
 {
     int number, mark;
     list* next;
+
+
+    void add(int nmbr, int mrk)
+    {
+        list* b = new list(number, mark, next);
+        mark =mrk;
+        number = nmbr;
+        next = b;
+    }
+    void add(int nmbr, int mrk, int pos, int count)
+    {
+        if(next == NULL)
+            return;
+        if(pos-1 == count || next->next == NULL)
+        {
+            list* b = new list(nmbr, mrk, next);
+            next = b;
+        }
+        else
+            next->add(nmbr,mrk,pos,count+1);
+    }
+
 public:
     list()
     {
@@ -32,12 +54,13 @@ public:
         o << number << '\t' << mark << endl;
         next->print(o);
     }
-    void add(int nmbr, int mrk)
+
+    void add(int nmbr, int mrk, int pos)
     {
-        list* b = new list(number, mark, next);
-        mark =mrk;
-        number = nmbr;
-        next = b;
+        if(pos==0)
+            add(nmbr, mrk);
+        else
+            add(nmbr, mrk, pos, 0);
     }
     void add(istream& a, ostream& o)
     {
@@ -73,15 +96,52 @@ public:
         }
         next->del(pos,i+1);
     }
+
+    bool great()
+    {
+        if(next->number > next->next->number)
+            return true;
+        return false;
+    }
+
+    void sort(int& len)
+    {
+        if(next->next->next==NULL)
+        {
+            return;
+        }
+        if(great())
+        {
+            list* tmp = next;
+            next = next->next;
+            tmp->next = next->next;
+            next->next = tmp;
+        }
+        len++;
+        next->sort(len);
+    }
+    void sort()
+    {
+        add(0,0,0);
+        int len=0, j;
+        sort(len);
+        j=len;
+        for(int i=0;i<j; i++) //Здесь можно оптимизировать
+            sort(len);
+        del(0);
+    }
 };
 
 int main()
 {
     list firstCourse;
-    firstCourse.add(cin, cout);
-    firstCourse.add(cin, cout);
-    firstCourse.add(cin, cout);
+    firstCourse.add(1, 1, 0);
+    firstCourse.add(2, 2, 0);
+    firstCourse.add(3, 3, 0);
+    firstCourse.add(12,1,12);
+    firstCourse.add(4, 3, 0);
     firstCourse.del(2);
+    firstCourse.sort();
     firstCourse.print(cout);
     return 0;
 }
