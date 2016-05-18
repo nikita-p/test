d55cd1c36bc6af2d9096d1e5d7c6c87d1555ec97
nikita-p <nikita.petrov.1997@gmail.com>
2016-05-18 08:17:07 +0600

    New view

--- experiment.cpp
+++ experiment.cpp
@@ -1,5 +1,5 @@
 #include <FL/Fl.H>
-#include <FL/Fl_Window.H>
+#include <FL/Fl_Double_Window.H>
 #include <FL/fl_draw.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Box.H>
@@ -13,11 +13,9 @@
 
 #define MAX_W 1000
 #define MAX_H 600
-#define G 100
 
 using namespace std;
-static int ForcePower; //Степень потенциала
-static float dt;
+static float ForcePower; //Степень при расстоянии в силе
 
 class CData
 {
@@ -37,20 +35,26 @@ public:
         Vx = 0;
         Vy = 0;
         M = m;
+    }
+    CData(int x, int y, float m, float vy)    {
+        X = x;
+        Y = y;
+        Vx = 0;
+        Vy = vy;
         M = m;
     }
     CData(const CData& t)  {
         this->X = t.X;
         this->Y = t.Y;
-        this->Vx = 0;
-        this->Vy = 0;
+        this->Vx = t.Vx;
+        this->Vy = t.Vy;
         this->M = t.M;
     }
     CData& operator = (const CData& t){
         this->X = t.X;
         this->Y = t.Y;
-        this->Vx = 0;
-        this->Vy = 0;
+        this->Vx = t.Vx;
+        this->Vy = t.Vy;
         this->M = t.M;
         return *this;
     }
@@ -63,7 +67,7 @@ public:
     float getVy()    {
         return Vy; }
     float getR()    {
-        return 10*M; }
+        return 20*M; }
     float getM()    {
         return M; }
     void setX(float m) {
@@ -78,7 +82,7 @@ public:
     void plusVy(float vy){
         Vy += vy;
     }
-    void changeCoordinats()
+    void changeCoordinats(float dt)
     {
         X = X + Vx * dt;
         Y = Y + Vy * dt;
@@ -86,52 +90,97 @@ public:
     friend class Circle;
 };
 
-class Circle : public Fl_Window
+class Circle : public Fl_Double_Window
 {
     vector<CData>* data;
+    float dt, G;
+    float zoom; //Масштаб
     void draw()
     {
-        Fl_Window::draw();
-        fl_color(FL_WHITE);
+        Fl_Double_Window::draw();
+        fl_color(FL_YELLOW);
         changeCoordinatsAndDraw();
     }
-
+    virtual int handle(int ev)
+        {
+          switch(ev) {
+            case FL_KEYUP:
+              if (Fl::event_key() == FL_Right) {
+                for(vector<CData>::iterator i = data->begin(); i!= data->end(); i++)
+                    (*i).X -= 10/zoom;
+                return 1;
+              }
+              if (Fl::event_key() == FL_Left) {
+                for(vector<CData>::iterator i = data->begin(); i!= data->end(); i++)
+                    (*i).X += 10/zoom;
+                return 1;
+              }
+              if (Fl::event_key() == FL_Up) {
+                for(vector<CData>::iterator i = data->begin(); i!= data->end(); i++)
+                    (*i).Y += 10/zoom;
+                return 1;
+              }
+              if (Fl::event_key() == FL_Down) {
+                for(vector<CData>::iterator i = data->begin(); i!= data->end(); i++)
+                    (*i).Y -= 10/zoom;
+                return 1;
+              }else {
+                return 0;
+              }
+            case FL_PUSH:
+              if(Fl::event_x() >0 && Fl::event_y() > 0){
+                    data->insert(data->begin(),CData(Fl::event_x()/zoom,Fl::event_y()/zoom,2));
+                    return 1;}
+          case FL_MOUSEWHEEL:{
+              float zoomOld = zoom;
+              if(Fl::event_dy() < 0){
+                  zoom = zoom*1.2;}
+              if(Fl::event_dy() > 0){
+                    zoom = zoom/1.2;  }
+              for(vector<CData>::iterator i = data->begin(); i!= data->end(); i++){
+                  (*i).X = (*i).X - Fl::event_x() * (1 - (zoomOld/zoom));
+                  (*i).Y = (*i).Y - Fl::event_y() * (1 - (zoomOld/zoom));}
+              return 0;
+          }
+            default:
+              return Fl_Double_Window::handle(ev);
+          }
+        }
 public:
-    Circle() : Fl_Window(MAX_W,MAX_H, "Tt") {Fl_Window::color(FL_BLACK);}
-    Circle(vector<CData>* v) : Fl_Window(MAX_W,MAX_H, "Tt")
+    Circle(vector<CData>* v) : Fl_Double_Window(MAX_W,MAX_H, "Tt")
     {
+        zoom = 1;
         data = v;
+        dt = 0.05;
+        G = 2*pow(10,-2*ForcePower-1)/dt;
         v->insert(v->end(),CData(1000*MAX_W, 1000*MAX_H,0)); // Так решена проблема, когда две последних налетают друг на друга
-        Fl_Window::color(FL_BLACK);
+        Fl_Double_Window::color(FL_BLACK);
     }
-    vector<CData>* getVector()
-    {
-        return data;
-    }
-    float dist (CData a, CData b)
-    {
-        return ((b.getY() - a.getY())*(b.getY() - a.getY() ) + (b.getX() - a.getX())*(b.getX() - a.getX()));
-    }
-    int sgn(float x, float y)
-    {
-        return ((y > x) - (y < x));
-    }
-    float TheCriticalDistance(vector<CData>::iterator i, vector<CData>::iterator j)
-    {
-        /*if((*i).getR()>(*j).getR())
-            return (*i).getR();
-        return (*j).getR();*/
-        return (*j).getR() + (*i).getR();
-    }
+    vector<CData>* getVector()    {
+        return data;    }
+    void addData(CData a){
+        data->insert(data->begin(),a);  }
+    float getdt()    {
+        return dt;    }
+    float getG()    {
+        return G;    }
+    float dist (CData a, CData b)    {
+        return sqrt((b.getY() - a.getY())*(b.getY() - a.getY() ) + (b.getX() - a.getX())*(b.getX() - a.getX()));    }
+    int sgn(float x, float y)    {
+        return ((y > x) - (y < x));    }
+    float TheCriticalDistance(vector<CData>::iterator i, vector<CData>::iterator j)    {
+        return (*j).getR() + (*i).getR();    }
     void impulse(vector<CData>::iterator i, vector<CData>::iterator j)
     {
         float M1 = (*i).getM();
-        float M2 = (*j).getM();
+        float M2 = (*j).getM();        
+        float Energy = M1*(pow((*i).Vx,2) + pow((*i).Vy,2)) + M2*(pow((*j).Vx,2) + pow((*j).Vy,2));
         float a = - atan(((*i).getY() - (*j).getY())/((*i).getX() - (*j).getX()));
         (*i).Vr = (*i).Vx * cos(a) - (*i).Vy * sin(a);
         (*i).Vt = (*i).Vx * sin(a) + (*i).Vy * cos(a);
         (*j).Vr = (*j).Vx * cos(a) - (*j).Vy * sin(a);
         (*j).Vt = (*j).Vx * sin(a) + (*j).Vy * cos(a);
+
         float V1 = (*i).Vr;
         float V2 = (*j).Vr;
         (*i).Vr = ((M1 - M2)*V1 + 2 * M2 * V2)/(M1+M2);
@@ -140,41 +189,60 @@ public:
         (*i).Vy = - (*i).Vr * sin(a) + (*i).Vt * cos(a);
         (*j).Vx = (*j).Vr * cos(a) + (*j).Vt * sin(a);
         (*j).Vy = - (*j).Vr * sin(a) + (*j).Vt * cos(a);
+/*
+        float k = sqrt((M1*(pow((*i).Vx,2) + pow((*i).Vy,2)) + M2*(pow((*j).Vx,2) + pow((*j).Vy,2)))/Energy);
+        cout << k <<endl;
+        (*i).Vx = (*i).Vx / k;
+        (*i).Vy = (*i).Vy / k;
+        (*j).Vx = (*j).Vx / k;
+        (*j).Vy = (*j).Vy / k;
+*/
     }
-
+    int sgn(float i)
+    {
+        if(i == 0)
+            return 0;
+        return 2*(i>0) - 1;
+    }
     void changeVelocity(vector<CData>::iterator i, vector<CData>::iterator j)
     {
-        if(sqrt(dist(*i, *j)) > TheCriticalDistance(i,j)){
-            float Force = G * (*i).getM() * (*j).getM() * ( pow(sqrt(dist(*i, *j)),ForcePower) ); //Force
-            (*i).plusVx( sgn((*i).getX(), (*j).getX())*Force*dt/(*i).getM() );
-            (*j).plusVx( - sgn((*i).getX(), (*j).getX())*Force*dt/(*j).getM() );
-            (*i).plusVy( sgn((*i).getY(), (*j).getY())*Force*dt/(*i).getM() );
-            (*j).plusVy( - sgn((*i).getY(), (*j).getY())*Force*dt/(*j).getM() );
+        if((dist(*i, *j)) > TheCriticalDistance(i,j)){
+            float Force = G * (*i).getM() * (*j).getM() * pow(dist(*i, *j),ForcePower); //Force
+            (*i).plusVx( - sgn(ForcePower)*sgn((*i).getX(), (*j).getX())*Force*dt/(*i).getM() );
+            (*j).plusVx( sgn(ForcePower)*sgn((*i).getX(), (*j).getX())*Force*dt/(*j).getM() );
+            (*i).plusVy( - sgn(ForcePower)*sgn((*i).getY(), (*j).getY())*Force*dt/(*i).getM() );
+            (*j).plusVy( sgn(ForcePower)*sgn((*i).getY(), (*j).getY())*Force*dt/(*j).getM() );
         }
         else
             impulse(i, j);
     }
-    void removeFarCirclesAndDraw()
+    void cintoc(vector<CData>::iterator i, vector<CData>::iterator j)
     {
-        int len = data->size() - 1; //Предупреждаю удаление фиктивного круга
-        vector<CData>::iterator i = data->begin();
-        for(int  k = 0; k < len; k++)
+        if(dist(*i, *j) < TheCriticalDistance(i,j))
         {
-            (*i).changeCoordinats();
-            fl_circle((*i).getX(), (*i).getY(), (*i).getR());
-            vector<CData>::iterator remove = i;
-            ++i;
-            //if((*remove).getX() > 5*MAX_W || (*remove).getX() < -5*MAX_W
-            //        || (*remove).getY() > 5*MAX_H || (*remove).getY() < -5*MAX_H)
-            //      data->erase(remove);
+            float normalX = (*i).getX() - (*j).getX();
+            float normalY = (*i).getY() - (*j).getY();
+            float sub = TheCriticalDistance(i,j) - dist(*i, *j);
+            float hipotenuza = sqrt(pow(normalX, 2) + pow(normalY,2));
+            normalX = normalX*sub/(2*hipotenuza);
+            normalY = normalY*sub/(2*hipotenuza);
+            (*i).X += normalX;
+            (*j).X -= normalX;
+            (*i).Y += normalY;
+            (*j).Y -= normalY;
         }
+        return;
     }
     void changeCoordinatsAndDraw()
     {
         for(vector<CData>::iterator i = data->begin(); i != data->end(); i++)
             for(vector<CData>::iterator j = i+1; j != data->end(); j++)
+            {
                 changeVelocity(i,j);
-        removeFarCirclesAndDraw();
+                (*i).changeCoordinats(dt);
+                cintoc(i, j); //Круг внутри круга
+                fl_circle((*i).getX()*zoom, (*i).getY()*zoom, (*i).getR()*zoom);
+            }
     }
 };
 
@@ -183,26 +251,26 @@ void callback(void* winp)
 {
     Circle* win =(Circle*)winp;
     win->redraw();
-    Fl::repeat_timeout(0.05,callback, win);
+    Fl::repeat_timeout(win->getdt(),callback, win);
 }
 
 vector<CData> createCircles()
 {
     vector<CData> a;
     a.reserve(10);
-    a.insert(a.end(),CData(540,300,2));
-    a.insert(a.end(),CData(340,100,1));
-    a.insert(a.end(),CData(200,400,4));
+    a.insert(a.end(),CData(300,600,2, -10));
+    a.insert(a.end(),CData(300,300,2, 10));
+    //a.insert(a.end(),CData(200,400,4));
     return a;
 }
-/*
+
 void b_call(Fl_Widget*, void* d)
 {
     pair<vector<CData>*, Fl_Slider** >* p = (pair<vector<CData>*, Fl_Slider** >*)d;
     Fl_Slider** s = p->second;
     p->first->insert(p->first->begin(),CData(s[0]->value(),s[1]->value(),s[2]->value()));
 }
-void newCircle(vector<CData>* a) //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+void newCircle(vector<CData>* a)
 {
     Fl_Slider** sliders = new Fl_Slider* [3];
     sliders[0] = new Fl_Slider(10,10,30,100, "X");
@@ -223,12 +291,10 @@ void play_callback(Fl_Widget* but, void*)
      if(string(b->label()) == "@||")
      {
          b->label("@>");
-         dt = 0;
      }
      else
      {
          b->label("@||");
-         dt = 1;
      }
 }
 
@@ -250,26 +316,56 @@ void force()
     pow->bounds(0,-3);
     pow->callback(slid_callback);
 }
-
 void drawSetup(vector<CData>* a)
 {
     int w = MAX_W/5;
     int h = MAX_H;
-    Fl_Window *controlPanel = new Fl_Window(0,0,w,MAX_H);
+    Fl_Double_Window *controlPanel = new Fl_Double_Window(0,0,w,MAX_H);
     controlPanel->color(FL_LIGHT2);
     newCircle(a);
     play();
     force();
 }
-*/
+
+void cc(Fl_Widget*, void* circle)
+{
+    Circle* c = (Circle*)circle;
+    c->addData(CData(300,100,2));
+}
+
+
+class ControlPanel
+{
+    Fl_Double_Window* win;
+    Fl_Button* addCircle;
+    Circle* cir;
+public:
+    ControlPanel(int x, int y, int W, int H, Circle* C)
+    {
+        win = new Fl_Double_Window(x,y,W,H);
+        win->color(FL_CYAN);
+        cir = C;
+    }
+    void createCircleButton(int x, int y, int W, int H)
+    {
+        Fl_Button* b = new Fl_Button(x,y,W,H, "+");
+        b->callback(cc, cir);
+    }
+};
+
+void setup(Circle* a)
+{
+    ControlPanel Contr(0,0,MAX_W/5,MAX_H, a);
+    Contr.createCircleButton(30,70,50,50);
+}
+
 int main()
-{    
-    dt = 1;
-    ForcePower = -1;
+{
+    ForcePower = -2;
     vector<CData> a = createCircles();
     Circle c(&a);
-    //drawSetup(&a);
-    Fl::add_timeout(0.1, callback, &c);
+    //setup(&c);
+    Fl::add_timeout(0.0, callback, &c);
     c.show();
     return(Fl::run());
 };
